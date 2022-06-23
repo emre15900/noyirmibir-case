b97bd7630eaec5e934814f642a2998e45f09001f
Emre Sayın <corabim.com1@gmail.com>
2022-06-23 20:50:11 +0300

    active navbar

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,34 +1,72 @@
 import React from "react";
 import styles from "./styles.module.css";
-import { Flex, Spacer, Box, Image } from "@chakra-ui/react";
+import { Flex, Spacer, Box, Image, color } from "@chakra-ui/react";
 import logo from "../../images/logo.svg";
 import { Link } from "react-scroll";
 import HamburgerMenu from "../Menu/HamburgerMenu";
+import "./active.css"
 
 function Header() {
+
   return (
     <div className={styles.header}>
       <Flex>
         <Box className={styles.header_logo}>
-          <Link to="/" spy={true} smooth={true} offset={-100} duration={500}>
+          <Link
+            to="show-all"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
             <Image src={logo} alt="product" />
           </Link>
         </Box>
         <Spacer />
         <Box className={styles.header_right}>
-          <Link to="show-all" spy={true} smooth={true} offset={-100} duration={500}>
+          <Link
+            to="show-all"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
             <Box p="4">Show All Tyres</Box>
           </Link>
-          <Link to="find-dealer" spy={true} smooth={true} offset={-100} duration={500} >
+          <Link
+            to="find-dealer"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
             <Box p="4">Find a Dealer</Box>
           </Link>
-          <Link to="guides-videos" spy={true} smooth={true} offset={-65} duration={500}>
+          <Link
+            to="guides-videos"
+            spy={true}
+            smooth={true}
+            offset={-65}
+            duration={500}
+          >
             <Box p="4">Guides & Videos</Box>
           </Link>
-          <Link to="/go-with" spy={true} smooth={true} offset={-100} duration={500}>
+          <Link
+            to="/go-with"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
             <Box p="4">Go with</Box>
           </Link>
-          <Link to="/service-help" spy={true} smooth={true} offset={-100} duration={500}>
+          <Link
+            to="/service-help"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
             <Box p="4">Service & Help</Box>
           </Link>
         </Box>
